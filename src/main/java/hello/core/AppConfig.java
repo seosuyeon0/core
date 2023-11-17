9b01b66c75ed98d221f6bd5bee67ee001a405c98
수연 <3437ssy@naver.com>
2023-11-17 23:28:44 +0900

    Order

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,41 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,17 +1,21 @@
 package hello.core.member;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map
+    private static Map<Long, Member> store = new HashMap<>();
+
+
     @Override
     public void save(Member member) {
-
+      store.put(member.getId(), member);
     }
 
     @Override
     public Member findById(Long memberId) {
-        return null;
+
+        return store.get(memberId);
     }
 }
