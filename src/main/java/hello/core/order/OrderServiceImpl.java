603c48ffb18b349fe1e3a9f72b86e26aec592a5a
수연 <3437ssy@naver.com>
2023-11-29 16:29:04 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,20 +5,18 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -29,7 +27,7 @@ public class OrderServiceImpl implements OrderService{
     }
 
     //테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+  //  public MemberRepository getMemberRepository() {
+    //    return memberRepository;
+    //}
 }
