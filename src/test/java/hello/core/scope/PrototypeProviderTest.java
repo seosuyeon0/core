c377e7bcaf1c3ee16518d22cedb013de3422271a
수연 <3437ssy@naver.com>
2023-12-05 16:21:18 +0900

    ObjectFactory, ObjectProvider

--- src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -24,9 +25,9 @@ public class PrototypeProviderTest {
     }
     static class ClientBean {
         @Autowired
-        private ApplicationContext ac;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
         public int logic() {
-            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
